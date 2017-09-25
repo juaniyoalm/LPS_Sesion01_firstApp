0bd637e89c5d85cceefda001243cd1e8839cc4a6
Juan Antonio Perez Clemente <juaniyoalm@MacBook-Pro-de-Juan.local>
2017-09-25 21:08:21 +0200

    Sesion 01 - Funcionalidad terminada

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: properties
 
+    @IBOutlet weak var nombreLbl: UILabel!
+    
+    @IBOutlet weak var nombreTxt: UITextField!
+    
+    @IBOutlet weak var apellidoTxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        nombreTxt.delegate = self
+        apellidoTxt.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Actions
+    
+    @IBAction func btnNuevo(_ sender: UIButton) {
+        nombreLbl.text = "Hola desconocido"
+        nombreTxt.text = ""
+        apellidoTxt.text = ""
+    }
+    
+    func getRandomColor() -> UIColor{
+        
+        let randomRed:CGFloat = CGFloat(drand48())
+        
+        let randomGreen:CGFloat = CGFloat(drand48())
+        
+        let randomBlue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+    
+    @IBAction func btnColour(_ sender: UIButton) {
+    
+        
+        self.view.backgroundColor = getRandomColor()
+    }
+    
+    
+    //MARK: UITextFIeldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if (nombreTxt.text != "" && apellidoTxt.text != "") {
+            nombreLbl.text = "Hola " + nombreTxt.text! + " " + apellidoTxt.text!
+        }
+        
+    }
+    
 }
 
